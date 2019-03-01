15bcd44e8410e3ef8762c0cd2818821f508f436d
TOSHIBA <TOSHIBA@192.168.1.60>
2019-03-01 06:03:19 -0500

    ok

--- Factorial/src/com/sergioh/factorial/Factorial.java
+++ Factorial/src/com/sergioh/factorial/Factorial.java
@@ -13,30 +13,31 @@ public class Factorial {
 	private static Scanner tc;
 
 	public static void main(String[] args) {
-		tc = new Scanner(System.in);//asistente de teclado
+		tc = new Scanner(System.in);// asistente de teclado
 		int n = 0;
 		double fac = 1;
 		System.out.println("INGRESANDO UN NUMERO SABRAS EL FACTORIAL DEL MISMO");
 		System.out.println();
 		System.out.print("Por favor ingrese un numero: ");
 		n = tc.nextInt();
+
 		if (n != 0) {
 			System.out.println("El numero descompuesto en digitos es: ");
-			for (int i = 0; i < n; i++) {      // descompone el numero
+			for (int i = 0; i < n; i++) { // descompone el numero
 				System.out.print((i + 1));
 
 			}
 
-			for (int i = 0; i < n; i++) {      //genera el factorial del numero
+			for (int i = 0; i < n; i++) { // genera el factorial del numero
 				fac *= i + 1;
 
 			}
 			System.out.println();
-			System.out.println("El factorial de " + n + " es: ");  //imprime el factorial
+			System.out.println("El factorial de " + n + " es: "); // imprime el factorial
 			System.out.println(fac);
 
 		} else {
-			System.out.println("EL cero no es un numero para el ejercicio.");   
+			System.out.println("EL cero no es un numero para el ejercicio.");
 
 		}
 
